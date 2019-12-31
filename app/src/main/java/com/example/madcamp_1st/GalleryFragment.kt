90a88dea798fb0e79ace99f780f2255f578a602f
tjfdk0410 <56718955+tjfdk0410@users.noreply.github.com>
2019-12-31 20:33:29 +0900

    00

--- app/src/main/java/com/example/madcamp_1st/GalleryFragment.kt
+++ app/src/main/java/com/example/madcamp_1st/GalleryFragment.kt
@@ -27,11 +27,16 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.gallery.*
 
+//image pick code
+private val IMAGE_PICK_CODE = 1000
+//Permission code
+private val PERMISSION_CODE = 1001
 
 class GalleryFragment: Fragment() {
 
     var imgList = arrayListOf<Image>()
-
+    private var SPAN_COUNT = 2
+    lateinit var galleryAdapter: GalleryRVAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,63 +44,25 @@ class GalleryFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        var view = inflater.inflate(
-            R.layout.gallery,
-            container,
-            false
-        ) //fragement 생성 위한 view를 gallery에서 띄우고 반환
-
-//        var rv: RecyclerView = view.findViewById(R.id.gallRecyclerView)
-////        var galleryAdapter = GalleryRVAdapter(requireContext(), imgList)
-////        println("11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111")
-////
-////        var SPAN_COUNT = 2
-////        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-////            SPAN_COUNT = 3
-////        }
-////
-////        //가로모드인 경우 col수 3개로 바꿈
-////
-////        val layoutManager = GridLayoutMana ger(requireContext(), SPAN_COUNT)
-////        rv.layoutManager = layoutManager
-////        rv.setHasFixedSize(true)
-////
-////        rv.adapter = galleryAdapter
+        var view = inflater.inflate(R.layout.gallery, container, false) //fragement 생성 위한 view를 gallery에서 띄우고 반환
         imgList.add(Image(Uri.parse("file://https://cdn.pixabay.com/photo/2019/12/23/15/07/freiburg-4714770_960_720.jpg")))
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        //var rv: RecyclerView = view.findViewById(R.id.gallRecyclerView)
-        var galleryAdapter = GalleryRVAdapter(context, imgList)
+        galleryAdapter = GalleryRVAdapter(requireContext(), imgList)
         gallRecyclerView.adapter = galleryAdapter
 
-        var SPAN_COUNT = 2
+        loadImages()
         if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
             SPAN_COUNT = 3
         }
-        //가로모드인 경우 col수 3개로 바꿈
 
         val layoutManager = GridLayoutManager(requireContext(), SPAN_COUNT)
-//        val layoutManager = LinearLayoutManager(requireContext())
         gallRecyclerView.layoutManager = layoutManager
         gallRecyclerView.setHasFixedSize(true)
-        getgallerypermission()
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-//        getgallerypermission()
-    }
 
 
-    /**
-     *  pickImageFromGallery()
-     *  get permission and call requestpermissions
-     *  if don't have to, call pickImageFromGallery(already get or less OS level)
-     */
-    private fun getgallerypermission() {
-//        print("pickkkkkkkkkkkk")
         img_pick_btn.setOnClickListener {
             //check runtime permission
             if (VERSION.SDK_INT >= VERSION_CODES.M) {
@@ -104,8 +71,7 @@ class GalleryFragment: Fragment() {
                         Manifest.permission.READ_EXTERNAL_STORAGE
                     ) ==
                     PackageManager.PERMISSION_DENIED
-                ) {
-                    //permission denied
+                ) { //permission denied
                     val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
                     //show popup to request runtime permission
                     requestPermissions(permissions, PERMISSION_CODE);
@@ -121,32 +87,26 @@ class GalleryFragment: Fragment() {
     }
 
 
+    private fun loadImages(){
+        imgList.add(Image(Uri.parse("https://cdn.pixabay.com/photo/2018/11/24/01/35/christmas-motif-3834860__340.jpg")))
+        imgList.add(Image(Uri.parse("https://cdn.pixabay.com/photo/2019/12/03/07/34/gift-4669449__340.jpg")))
+        imgList.add(Image(Uri.parse("https://cdn.pixabay.com/photo/2016/10/30/09/30/hot-chocolate-1782623__340.jpg")))
+        galleryAdapter.notifyDataSetChanged()
+    }
+
+
     /**
      *  pickImageFromGallery()
      *  through intent, call gallery activity
      *  go to gallery activity, and get some image
      */
     private fun pickImageFromGallery() {
-//        println("ffff")
-        //Intent to pick image
-//        val intent = Intent(Intent.ACTION_PICK)
-//        intent.type = "image/*"
-//        startActivityForResult(intent, IMAGE_PICK_CODE)
-        val intent = Intent(Intent.ACTION_PICK)
-        //action_pick이면 구글 드라이브로 가는데?????? action_content하면 안뜨고ㅜㅜ
+        val intent = Intent(Intent.ACTION_PICK) //action get content 고려
         intent.setType("image/*")
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true) //activity 간의 인수와 리턴값을 전달(저장)
         startActivityForResult(Intent.createChooser(intent, "Select picture"), IMAGE_PICK_CODE)
-
     }
 
-    companion object {
-        //
-        //image pick code
-        private val IMAGE_PICK_CODE = 1000;
-        //Permission code
-        private val PERMISSION_CODE = 1001;
-    }
 
     /**
      *  onRequestPermissionResult()
@@ -182,130 +142,19 @@ class GalleryFragment: Fragment() {
      *  handle result of picked image
      */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
-//            image_view.setImageURI(data?.data)
+
             if (data == null) {
                 //something is wrong
             }
-//            else {
-//                val uri = data.data
-//                if (uri != null) {
-//                    imgList.add(uri)
-//                }
-//            }
             val clipData = data?.clipData
-//            val a = clipData?.getItemAt(0)
-//            imgList = imgList
             if (clipData != null) {
                 for (i in 0 until clipData.itemCount) {
                     val str = clipData.getItemAt(i).uri
                     imgList.add(Image(str))
                 }
             }
-
+            galleryAdapter.notifyDataSetChanged()
         }
-
     }
 }
-
-//            Toast.makeText(requireContext(), "$clipData", Toast.LENGTH_LONG)
-//            if (clipData != null){
-//                for (i in 0 until clipData.itemCount-1){
-//                    val uri = clipData.getItemAt(i).uri
-//                    importPhoto(uri)
-//                }
-//            } else {
-//                val uri = data?.data
-//                if (uri != null) {
-//                    importPhoto(uri)
-//                }
-//            }
-
-
-
-
-
-//
-//    /**
-//     *  importPhoto()
-//     *  not image, false
-//     *  try make file, get URi
-//     */
-//    fun importPhoto(uri: Uri): Boolean {
-//        if (!isImage(requireContext(), uri)) {
-//            // not image
-//            return false
-//        }
-//
-//        return try {
-//            val photoFile = createImageFile(requireContext().cacheDir)
-//            //cacheDir: provide a directory where the OS want to store cached files
-//            copyUriToFile(requireContext(), uri, photoFile)
-//
-//            // addImageToGallery(photoFile)
-//            true
-//        }
-//        catch (e: IOException) {
-//            e.printStackTrace()
-//            // handle error
-//            false
-//        }
-//    }
-//
-//
-//    /**
-//     *  isImage()
-//     *  와 씨 진짜 모르겠다ㅜ
-//     *
-//     */
-//    fun isImage(context: Context, uri: Uri): Boolean {
-//        val mimeType = context.contentResolver.getType(uri) ?: return true
-//        //MIME : Multiple purpose internet mail extension => 여러 형태의 파일 전달 위해 사용
-//        //인코딩 방식을 말한다 텍스트로 변환 인코딩, 바이너리로 전환 디코딩
-//        return mimeType.startsWith("image/")
-//    }
-//    //서버 대신하는 안드로이드 4대 컴포넌트 중 하나인 content provider 가서 가져오는 듯
-//
-//    /**
-//     *  createImageFile()
-//     *  make file for pattern("yyyyMMdd-kkmmss")
-//     *  if
-//     */
-//    private fun createImageFile(dir: File, fileName: String? = null): File {
-//        if (fileName == null) {
-//            val timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-kkmmss"))
-//            return File.createTempFile("IMG_$timestamp", ".jpg", dir)
-//            // 임시파일 생성
-//        }
-//        return File(dir, fileName)
-//    }
-//
-//    /**
-//     *  copyUriToFile()
-//     *
-//     *
-//     */
-//    fun copyUriToFile(context: Context, uri: Uri, outputFile: File) {
-//        val inputStream = context.contentResolver.openInputStream(uri)
-//
-//        // copy inputStream to file using okio
-//        /*
-//        val source = Okio.buffer(Okio.source(inputStream))
-//        val sink = Okio.buffer(Okio.sink(outputFile))
-//
-//        sink.writeAll(source)
-//
-//        sink.close()
-//        source.close()
-//         */
-//
-//        val outputStream = FileOutputStream(outputFile)
-//        inputStream.use { input ->
-//            outputStream.use { output ->
-//                input?.copyTo(output)
-//            }
-//        }
-//    }
-
-
--- app/src/main/java/com/example/madcamp_1st/GalleryRVAdapter.kt
+++ app/src/main/java/com/example/madcamp_1st/GalleryRVAdapter.kt
@@ -10,30 +10,30 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.gallery.*
 
-class GalleryRVAdapter(val context : Context?, val itemList:ArrayList<Image>) :
-    RecyclerView.Adapter<GalleryRVAdapter.Holder>() {
+class GalleryRVAdapter(val context : Context?, val itemList:ArrayList<Image>) : RecyclerView.Adapter<GalleryRVAdapter.Holder>() {
 
-    //화면 최초 로딩, view가 없으면 xml파일을 inflate하여 ViewHolder를 생성한다.
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val view = LayoutInflater.from(context).inflate(R.layout.gallery_rv_item, parent, false)
         return Holder(view)
     }
 
-    //RecyclerView로 만들어지는 item의 총 개수를 반환한다.
     override fun getItemCount(): Int {
         return itemList.size
     }
 
-    //위의 onCreateViewHolder에서 만든 view와 실제 입력되는 각각의 데이터를 연결한다.
     override fun onBindViewHolder(holder: Holder, position: Int) {
+        val gallPhoto = holder.itemView.findViewById<ImageView>(R.id.gallphoto)
+        gallPhoto.setImageURI(itemList.get(position).uri)
         holder.bind(itemList.get(position))
     }
 
     inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val gallPhoto = itemView.findViewById<ImageView>(R.id.gallphoto)
-        //종류 정하기, layout과 연결
+//        gallPhoto.setImageURI(Im.uri)
+
         fun bind(Im: Image) {
 //            for (i in itemList) {
+        //            println("11111111111111111111111111111111111111111"+Im.uri)
             gallPhoto.setImageURI(Im.uri)
 //            notifyDataSetChanged()
 //            itemView.setOnClickListener{itemClick(Im)}
